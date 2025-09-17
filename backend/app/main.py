137c940accf2b1f65cd3d9c56a27f7c8ed9adb25
adnankhan171 <adnankhan17371@gmail.com>
2025-09-17 17:56:24 +0530

    time function updated,slowapi commented

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,12 @@
 # app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from slowapi.middleware import SlowAPIMiddleware
-from slowapi.errors import RateLimitExceeded
-from slowapi import _rate_limit_exceeded_handler
+# from slowapi.middleware import SlowAPIMiddleware
+# from slowapi.errors import RateLimitExceeded
+# from slowapi import _rate_limit_exceeded_handler
 
 from .db import init_db
-from .libs.limiter import limiter
+# from .libs.limiter import limiter
 
 from .routers import auth_router, posts_router, comments_router, likes_router
 
@@ -15,8 +15,8 @@ app = FastAPI(title="Comment System API")
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "http://localhost:5173",# This is the default port for React development server
-    # "https://your-production-frontend.com" # Add your production domain here
+    "http://localhost:5173",
+    
 ]
 # Add the CORS middleware to the application
 app.add_middleware(
@@ -26,10 +26,10 @@ app.add_middleware(
     allow_methods=["*"], # Allows all HTTP methods (GET, POST, PUT, DELETE, OPTIONS, etc.)
     allow_headers=["*"], # Allows all headers
 )
-# attach limiter/middleware
-app.state.limiter = limiter
-app.add_middleware(SlowAPIMiddleware)
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+# # attach limiter/middleware
+# app.state.limiter = limiter
+# app.add_middleware(SlowAPIMiddleware)
+# app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 # include routers
 app.include_router(auth_router.router)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,7 @@
 # app/models.py
 from sqlmodel import SQLModel, Field
 from typing import Optional
-from datetime import datetime
+from datetime import datetime, timezone
 from sqlalchemy import Column, String, Boolean, UniqueConstraint
 
 class User(SQLModel, table=True):
@@ -9,14 +9,14 @@ class User(SQLModel, table=True):
     username: str = Field(sa_column=Column("username", String, nullable=False, unique=True)) 
     email: str = Field(sa_column=Column("email", String, nullable=False, unique=True))
     password_hash: str
-    created_at: datetime=Field(default_factory=datetime.utcnow)
+    created_at: datetime=Field(default_factory=datetime.now(timezone.utc))
     
 class Post(SQLModel, table=True):
     id: Optional[int]= Field(default=None, primary_key=True)
     author_id: int = Field(foreign_key="user.id", nullable=False, index=True)
     title: str
     content: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=datetime.now(timezone.utc))
     
 class Comment(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -25,14 +25,14 @@ class Comment(SQLModel, table=True):
     parent_id: Optional[int] = Field(default=None, foreign_key="comment.id", index=True)
     content: str
     deleted: bool= Field(default=False)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=datetime.now(timezone.utc))
     
 class CommentLike(SQLModel, table=True):
     __table_args__ = (UniqueConstraint("comment_id","user_id",name='uq_comment_user'),)
     id: Optional[int] = Field(default=None, primary_key=True)
     comment_id: int = Field(foreign_key="comment.id", nullable=False, index=True)
     user_id: int = Field(foreign_key="user.id", nullable=False, index=True)
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=datetime.now(timezone.utc))
     
     
     
